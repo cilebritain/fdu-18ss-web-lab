b218420d4e630f70fee7f20d30ff2c42a93a66d4
cilebritain <2023697797@qq.com>
2019-06-10 18:29:51 +0800

    complete lab11

--- lab11/Lab11.php
+++ lab11/Lab11.php
@@ -3,8 +3,7 @@
 
 //****** Hint ******
 //connect database and fetch data here
-
-
+$mysqli=new mysqli('localhost','root','r00t','travel');
 ?>
 
 <!DOCTYPE html>
@@ -39,7 +38,7 @@
           <div class="panel-body">
             <form action="Lab11.php" method="get" class="form-horizontal">
               <div class="form-inline">
-              <select name="continent" class="form-control">
+              <select name="continent" class="form-control" >
                 <option value="0">Select Continent</option>
                 <?php
                 //Fill this place
@@ -47,6 +46,8 @@
                 //****** Hint ******
                 //display the list of continents
 
+                $sql='SELECT * FROM continents';
+                $result=$mysqli->query($sql);
                 while($row = $result->fetch_assoc()) {
                   echo '<option value=' . $row['ContinentCode'] . '>' . $row['ContinentName'] . '</option>';
                 }
@@ -58,12 +59,16 @@
                 <option value="0">Select Country</option>
                 <?php 
                 //Fill this place
-
+                $sql='SELECT * FROM countries';
+                $result=$mysqli->query($sql);
+                while($row = $result->fetch_assoc()) {
+                  echo '<option value=' . $row['ISO'] . '>' . $row['CountryName'] . '</option>';
+                }
                 //****** Hint ******
                 /* display list of countries */ 
                 ?>
               </select>    
-              <input type="text"  placeholder="Search title" class="form-control" name=title>
+              <input type="text"  placeholder="Search title" class="form-control" name="title">
               <button type="submit" class="btn btn-primary">Filter</button>
               </div>
             </form>
@@ -76,19 +81,30 @@
             <?php 
             //Fill this place
 
+            $continent=$_GET['continent'];
+            $country=$_GET['country'];
+            $title=$_GET['title'];
+
+            if($continent AND $country){
+                $sql='SELECT * FROM imagedetails WHERE ContinentCode="'.$continent.'" AND CountryCodeISO="'.$country.'"';
+            }else{
+                $sql='SELECT * FROM imagedetails';
+            }
+            $result=$mysqli->query($sql);
+
+            while($row = $result->fetch_assoc()) {
+                echo '<li>';
+                echo '<a href="detail.php?id=????" class="img-responsive">';
+                echo '<img src="images/square-medium/'.$row['Path'].'" alt="'.$row['Title'].'" style="width:225px; height:225px;">';
+                echo '<div class="caption">';
+                echo '<div class="blur"></div>';
+                echo '<div class="caption-text">';
+                echo '<p>'.$row['Title'].'</p>';
+                echo '</div></div></a></li>';        
+              }
+
             //****** Hint ******
             /* use while loop to display images that meet requirements ... sample below ... replace ???? with field data
-            <li>
-              <a href="detail.php?id=????" class="img-responsive">
-                <img src="images/square-medium/????" alt="????">
-                <div class="caption">
-                  <div class="blur"></div>
-                  <div class="caption-text">
-                    <p>????</p>
-                  </div>
-                </div>
-              </a>
-            </li>        
             */ 
             ?>
        </ul>       
